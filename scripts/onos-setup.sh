61d1fb300fda1bc08dfdb56f4797979b1ed5a285
Bishwa K. Thapa <thapabishwakranti@gmail.com>
2018-08-01 12:08:27 +0200

    Changes to be committed:

---
+++ scripts/onos-setup.sh
@@ -0,0 +1,24 @@
+onos_version=${1}
+
+function usage {
+	echo "Usage: onos-setup <onos-version>" >&2
+	echo "*supported ONOS version: 1.6, 1.9, 1.11" >&2
+}
+case $onos_version in
+	1.6|1.6.0)
+		echo "Setting up ONOS 1.6.0 to AppAgent..."
+    	onos_version=1.6
+        ./delta-setup-onos $onos_version
+   	;;
+    1.9|1.9.0)
+		echo "Setting up ONOS 1.9.0 to AppAgent..."
+    	onos_version=1.9
+        ./delta-setup-onos $onos_version
+  	;;
+    1.11|1.11.0)
+        echo "Setting up ONOS 1.11.0 to AppAgent..."
+        onos_version=1.11
+       ./delta-setup-onos $onos_version
+        ;;
+   *) usage;;
+esac
--- scripts/onos.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-sudo apt-get install git -y
-#Add onos sources
-
-git clone # TODO: Add ONOS repo and blah blah
-cd onos
-buck build onos
-cd ;
